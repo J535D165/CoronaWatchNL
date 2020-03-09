9fbb853ab856000f437d094bb3b621181694f5f8
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-03-09 22:42:39 +0100

    Fix incorrect chart

--- plots.R
+++ plots.R
@@ -9,14 +9,14 @@ data_daily = read_csv("data/rivm_corona_in_nl_daily.csv")
 data_daily %>%
   add_row(Datum = "2020-02-26", Aantal = 0) %>%
   ggplot(aes(Datum, Aantal)) +
-    geom_line() + 
-    ylim(0, NA) + 
-    # xlim(as.Date("2020-02-27"), NA) + 
-    theme_minimal() + 
-    theme(axis.title.x=element_blank(),
-          axis.title.y=element_blank()) + 
-    ggtitle("Aantal Coronavirus besmettingen") + 
-    ggsave("plots/timeline.png", width = 6, height=4)
+  geom_line() + 
+  ylim(0, NA) + 
+  # xlim(as.Date("2020-02-27"), NA) + 
+  theme_minimal() + 
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank()) + 
+  ggtitle("Aantal Coronavirus besmettingen") + 
+  ggsave("plots/timeline.png", width = 6, height=4)
 
 
 ### Top 10 municipalities
@@ -34,12 +34,12 @@ data %>%
   arrange(desc(Datum), desc(Aantal)) %>%
   mutate(Gemeente = factor(Gemeente, levels=pull(top_10_municipalities, Gemeente))) %>%
   ggplot(aes(Datum, Aantal, color=Gemeente)) +
-    geom_line() + 
-    theme_minimal() + 
-    theme(axis.title.x=element_blank(),
-          axis.title.y=element_blank()) + 
-    ggtitle("Gemeentes met meeste Coronavirus besmettingen") + 
-    ggsave("plots/top_municipalities.png", width = 6, height=4)
+  geom_line() + 
+  theme_minimal() + 
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank()) + 
+  ggtitle("Gemeentes met meeste Coronavirus besmettingen") + 
+  ggsave("plots/top_municipalities.png", width = 6, height=4)
 
 ### Per province
 
@@ -52,15 +52,15 @@ data %>% left_join(
   select(mun, Gemeentecode, Provincienaam),
   by=c("id"="Gemeentecode")
 ) %>% 
-  filter(Datum == max(data$Datum)) %>%
-  ggplot(aes(Provincienaam)) + 
-    geom_bar() + 
-    theme_minimal() + 
-    theme(axis.text.x=element_text(angle=45,hjust=1,vjust=1.1)) +
-    theme(axis.title.x=element_blank(),
-          axis.title.y=element_blank()) + 
-    ggtitle("Coronavirus besmettingen per provincie") + 
-    ggsave("plots/province_count.png", width = 6, height=4)
+  filter(Datum == max(Datum)) %>%
+  ggplot(aes(Provincienaam, Aantal)) + 
+  geom_col() + 
+  theme_minimal() + 
+  theme(axis.text.x=element_text(angle=45,hjust=1,vjust=1.1)) +
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank()) + 
+  ggtitle("Coronavirus besmettingen per provincie") + 
+  ggsave("plots/province_count.png", width = 6, height=4)
 
 
 
@@ -70,12 +70,12 @@ data %>% left_join(
 ) %>% group_by(Provincienaam, Datum) %>%
   summarise(Aantal = sum(Aantal, na.rm = T)) %>% 
   ggplot(aes(Datum, Aantal, color=Provincienaam)) + 
-    geom_line() + 
-    theme_minimal() + 
-    ggtitle("Coronavirus besmettingen per provincie") +     
-    theme(axis.title.x=element_blank(),
-          axis.title.y=element_blank()) + 
-    ggsave("plots/province_count_time.png", width = 6, height=4)
+  geom_line() + 
+  theme_minimal() + 
+  ggtitle("Coronavirus besmettingen per provincie") +     
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank()) + 
+  ggsave("plots/province_count_time.png", width = 6, height=4)
 
 
 ### Predictions
@@ -96,9 +96,9 @@ ggplot() +
   geom_line(aes(timevalues, Counts.exponential), color="red") + 
   geom_point(aes(Datum, Aantal), data = data_daily_ext) + 
   theme_minimal() + 
-    ggtitle("Voorspelling aantal Coronavirus besmettingen") +     
-    theme(axis.title.x=element_blank(),
-          axis.title.y=element_blank()) + 
+  ggtitle("Voorspelling aantal Coronavirus besmettingen") +     
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank()) + 
   ggsave("plots/prediction.png", width = 6, height=4)
 
 ## stats
