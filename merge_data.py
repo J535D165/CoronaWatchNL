c635dea8595c86b50daa9f525f15bd394b490b86
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-03-24 15:11:04 +0100

    Add data 24 maart and update plots

--- merge_data.py
+++ merge_data.py
@@ -154,6 +154,8 @@ if __name__ == '__main__':
             df_day = parse_new_format(file, n_missing=155)  # wrong number in data file (55)
         elif file == Path('raw_data') / "peildatum-23-maart-14-30.csv":
             df_day = parse_new_format(file, n_missing=184)
+        elif file == Path('raw_data') / "peildatum-24-maart-14-00.csv":
+            df_day = parse_new_format(file, n_missing=200)
         else:
             df_day = parse_new_format(file)
 
