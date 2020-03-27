f4b40ed781db3c3c7ad52d7c76141c1abef1ea33
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-03-27 14:49:07 +0100

    Data and plots for 27 march

--- merge_data.py
+++ merge_data.py
@@ -160,6 +160,8 @@ if __name__ == '__main__':
             df_day = parse_new_format(file, n_missing=213)
         elif file == Path('raw_data') / "peildatum-26-maart-14-00.csv":
             df_day = parse_new_format(file, n_missing=237)
+        elif file == Path('raw_data') / "peildatum-27-maart-14-00.csv":
+            df_day = parse_new_format(file, n_missing=265)
         else:
             df_day = parse_new_format(file)
 
