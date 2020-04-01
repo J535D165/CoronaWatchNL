da73cf43505103348ba6a64d8d634cabf9f6a274
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-04-01 11:19:40 +0200

    Fix missing data point and update plots

--- plot_scripts/plots.R
+++ plot_scripts/plots.R
@@ -39,6 +39,7 @@ daily_diff <- data_daily %>%
 
 
 daily %>%
+  mutate(meas = factor(meas, c("Totaal", "Ziekenhuisopname", "Overleden"))) %>%
   ggplot(aes(x = Datum, y = Aantal, colour = meas)) +
   geom_line() + 
   theme_minimal() + 
@@ -52,6 +53,7 @@ daily %>%
 
 
 daily_diff %>%
+  mutate(meas = factor(meas, c("Totaal", "Ziekenhuisopname", "Overleden"))) %>%
   ggplot(aes(x = Datum, y = Aantal, colour = meas)) +
   geom_line() + 
   theme_minimal() + 
@@ -79,6 +81,30 @@ read_csv("data/rivm_NL_covid19_sex.csv") %>%
   ggtitle("Covid19 patiënten per geslacht") +
   ggsave("plots/overview_plot_geslacht.png", width = 5, height=4)
 
+
+
+# plot age
+
+read_csv("data/rivm_NL_covid19_age.csv") %>%
+  filter(LeeftijdGroep != "Niet vermeld") %>%
+  mutate(
+    groep = ifelse(LeeftijdGroep %in% c("0-4", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59"), "0-59", NA),
+    groep = ifelse(LeeftijdGroep %in% c("60-64", "65-69", "70-74"), "60-74", groep),
+    groep = ifelse(LeeftijdGroep %in% c("75-79", "80-84", "85-89", "90-94", "95+"), "75+", groep)
+  ) %>% 
+  group_by(Datum, groep, Type) %>% 
+  summarize(Aantal = sum(Aantal)) %>%
+  mutate(Type = factor(Type, c("Totaal", "Ziekenhuisopname", "Overleden"))) %>%
+  ggplot(aes(x = Datum, y = Aantal, colour = Type, linetype=groep)) +
+  geom_line() + 
+  theme_minimal() + 
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        legend.pos = "bottom",
+        legend.title = element_blank()) +
+  scale_color_manual(values=c("#E69F00", "#56B4E9", "#999999")) + 
+  ggtitle("Covid19 patiënten per leeftijd") +
+  ggsave("plots/overview_plot_leeftijd.png", width = 5, height=4)
 
 
 # plot with countermeasures
