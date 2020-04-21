99cd5fbd0f53ed7d87b2f701d9b7753eea202afe
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-04-21 14:51:45 +0200

    Add data and plots 2020-04-21

--- data_collection.sh
+++ data_collection.sh
@@ -3,5 +3,5 @@ python download_rivm_map_data.py
 python merge_data.py
 python merge_website_charts.py
 python nice_download_merge.py
-pdftotext ../reports/reports/COVID-19_epidemiological_report_20200420.pdf
+pdftotext ../reports/reports/COVID-19_epidemiological_report_20200421.pdf
 python parse_pdf_table.py
