bb5292eefe92410f03f97bc3b644a760183736f5
RVoor <63646486+RVoor@users.noreply.github.com>
2020-04-30 15:44:06 +0200

    Add sex ratio plots

--- plot_scripts/plots_descriptive-data.R
+++ plot_scripts/plots_descriptive-data.R
@@ -31,7 +31,8 @@ read_csv("data/rivm_NL_covid19_age.csv") %>%
   ggsave("plots/overview_plot_leeftijd.png", width = 5.5, height=4)
 
 # Sex
-# Totaal
+# COVID-19 patiënten per geslacht
+## Absolute waarden
 read_csv("data/rivm_NL_covid19_sex.csv") %>%
   filter(Geslacht %in% c("Man", "Vrouw")) %>%
   mutate(Type = factor(Type, c("Totaal", "Ziekenhuisopname", "Overleden"))) %>%
@@ -46,28 +47,49 @@ read_csv("data/rivm_NL_covid19_sex.csv") %>%
   ggtitle("COVID-19 patiënten per geslacht") +
   ggsave("plots/overview_plot_geslacht.png", width = 5.5, height=4)
 
-# Toename
-age <- read_csv("data/rivm_NL_covid19_sex.csv")
-age <- age %>%
+## Percentages
+sex <- read_csv("data/rivm_NL_covid19_sex.csv") 
+sex <- sex %>% filter(Geslacht != "Niet vermeld")
+ratio <- (sex %>% filter(Geslacht == "Man")   %>% select(Aantal) /
+          sex %>% filter(Geslacht == "Vrouw") %>% select(Aantal))
+sex['Ratio'] <- rep(as.matrix(ratio), each =2)
+
+sex %>%
+  mutate(Type = factor(Type, c("Totaal", "Ziekenhuisopname", "Overleden"))) %>%
+  ggplot(aes(x = Datum, y = Ratio, colour = Type)) +
+  geom_line() +
+  theme_minimal() +
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        legend.pos = "bottom",
+        legend.title = element_blank()) +
+  scale_color_manual(values=c("#E69F00", "#56B4E9", "#999999")) +
+  ggtitle("COVID-19 patiënten Man:Vrouw ratio") +
+  ggsave("plots/ratio_plot_geslacht.png", width = 5.5, height=4)
+
+
+# TOENAME COVID-19 patiënten per geslacht 
+## Absolute waarden
+sex <- sex %>% 
   filter(Geslacht == "Man" & Type == "Totaal") %>%
   mutate(Toename = Aantal - lag(Aantal)) %>%
-  bind_rows(age %>%
+  bind_rows(sex %>% 
               filter(Geslacht == "Man" & Type == "Ziekenhuisopname") %>%
               mutate(Toename = Aantal - lag(Aantal))) %>%
-  bind_rows(age %>%
+  bind_rows(sex %>% 
               filter(Geslacht == "Man" & Type == "Overleden") %>%
               mutate(Toename = Aantal - lag(Aantal))) %>%
-  bind_rows(age %>%
+  bind_rows(sex %>% 
               filter(Geslacht == "Vrouw" & Type == "Totaal") %>%
               mutate(Toename = Aantal - lag(Aantal))) %>%
-  bind_rows(age %>%
+  bind_rows(sex %>% 
               filter(Geslacht == "Vrouw" & Type == "Ziekenhuisopname") %>%
               mutate(Toename = Aantal - lag(Aantal))) %>%
-  bind_rows(age %>%
+  bind_rows(sex %>% 
               filter(Geslacht == "Vrouw" & Type == "Overleden") %>%
               mutate(Toename = Aantal - lag(Aantal)))
 
-age %>%
+sex %>%
   mutate(Type = factor(Type, c("Totaal", "Ziekenhuisopname", "Overleden"))) %>%
   ggplot(aes(x = Datum, y = Toename, colour = Type, linetype= Geslacht)) +
   geom_line() +
@@ -79,3 +101,23 @@ age %>%
   scale_color_manual(values=c("#E69F00", "#56B4E9", "#999999")) +
   ggtitle("Toename COVID-19 patiënten per geslacht") +
   ggsave("plots/toename_plot_geslacht.png", width = 5.5, height=4)
+
+
+## Percentages
+sex <- sex[order(sex$Datum, sex$Type),]
+ratio_toe <- (sex %>% filter(Geslacht == "Man")   %>% select(Toename) /
+                sex %>% filter(Geslacht == "Vrouw") %>% select(Toename))
+sex['Ratio_toe'] <- rep(as.matrix(ratio_toe), each =2)
+
+sex %>%
+  mutate(Type = factor(Type, c("Totaal", "Ziekenhuisopname", "Overleden"))) %>%
+  ggplot(aes(x = Datum, y = Ratio_toe, colour = Type)) +
+  geom_line() +
+  theme_minimal() +
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        legend.pos = "bottom",
+        legend.title = element_blank()) +
+  scale_color_manual(values=c("#E69F00", "#56B4E9", "#999999")) +
+  ggtitle("Toename COVID-19 patiënten Man:Vrouw ratio") +
+  ggsave("plots/ratio_toename_geslacht.png", width = 5.5, height=4)
