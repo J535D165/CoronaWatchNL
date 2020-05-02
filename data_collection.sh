b39ebeae4a52fc88e66ab49fd90218bee70d42c7
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-05-02 14:30:19 +0200

    Add data 2020-05-02

--- data_collection.sh
+++ data_collection.sh
@@ -5,7 +5,7 @@ python merge_website_charts.py
 python nice_download_merge.py
 python data_rivm_geo.py
 python json-api.py
-pdftotext reports/COVID-19_epidemiological_report_20200428.pdf
+pdftotext reports/COVID-19_epidemiological_report_20200502.pdf
 python parse_pdf_table.py
 
 
