6514822a86778214729c177adddb23347bfd9c21
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-05-04 09:41:04 +0200

    New dataset with LCPS counts with country of hospitalisation (#97)

--- lcps.py
+++ lcps.py
@@ -1,9 +1,13 @@
-from retry_requests import retry
-from requests import Session
-from datetime import datetime
-import locale
-import pandas as pd
 import dateparser
+from datetime import datetime
+import re
+
+from requests import Session
+from retry_requests import retry
+import pandas as pd
+
+
+LCPS_API_URL = 'https://s3.eu-de.cloud-object-storage.appdomain.cloud/cloud-object-storage-lcps/news.json'  # noqa
 
 
 def get(url):
@@ -16,8 +20,10 @@ def get(url):
 
     return ret
 
+
 def titlenormalizer(title):
     return ' '.join(title.split(' ')[0:2]).replace('.', '').lower().strip()
+
 
 def titleclassifier(title):
     split = title.split(' ')
@@ -26,7 +32,7 @@ def titleclassifier(title):
         return False
 
     try:
-        retval = int(split[0])
+
         if split[1].startswith('covid') or split[1].startswith('corona'):
             return True
 
@@ -35,33 +41,64 @@ def titleclassifier(title):
 
     return False
 
-# locale.setlocale(locale.LC_TIME, 'nl_NL.utf8')  # set locale to netherlands for date parsing
 
-news = get('https://s3.eu-de.cloud-object-storage.appdomain.cloud/cloud-object-storage-lcps/news.json').json()
+if __name__ == '__main__':
 
-year = datetime.now().year
+    news = get(LCPS_API_URL).json()
 
-data = []
+    year = datetime.now().year
 
-for item in news['updates']:
-    title = titlenormalizer(item['title'])
-    if titleclassifier(title):
-        patients = int(title.split(' ')[0])
+    data = []
 
-        date = str(dateparser.parse(item['date'], languages=["nl"]).date())
-        data.append({'Date': date, 'Aantal': patients})
+    for item in news['updates']:
 
+        # print(item["content"])
 
-df_parsed_nums = pd.DataFrame(data).set_index('Date').sort_index()
+        title = titlenormalizer(item['title'])
 
-df_lcps = pd.read_csv('data/lcps_ic.csv', index_col=0)
+        # print(item['content'])
+        if titleclassifier(title):
+            patients = int(title.split(' ')[0])
 
-df_lcps = df_lcps.combine_first(df_parsed_nums)
-df_lcps['Aantal'] = df_lcps['Aantal'].astype(int)
+            date = str(dateparser.parse(item['date'], languages=["nl"]).date())
 
-# -- this only needs to run once
-# -- to drop duplicates
-df_lcps.drop_duplicates(inplace=True)
-# -- end
+            matches = re.search(
+                r"(\d+)\s+in\s+Duitsland",
+                item['content'],
+                re.MULTILINE
+            )
+            patients_in_de = None
+            try:
+                patients_in_de = int(matches.group(1))
+            except AttributeError:
+                pass
 
-df_lcps.to_csv('data/lcps_ic.csv')
+            data.append({'Date': date, 'Aantal': patients,
+                         'AantalDuitsland': patients_in_de})
+
+    df_parsed_nums = pd.DataFrame(data).set_index('Date').sort_index()
+
+    df_lcps = pd.read_csv('data/lcps_ic.csv', index_col=0)
+
+    df_lcps = df_lcps.combine_first(df_parsed_nums)
+    df_lcps['Aantal'] = df_lcps['Aantal'].astype(pd.Int64Dtype())
+    df_lcps['AantalDuitsland'] = df_lcps['AantalDuitsland'].astype(pd.Int64Dtype())
+
+    df_lcps[~df_lcps.index.duplicated()]
+    df_lcps[["Aantal"]].to_csv('data/lcps_ic.csv')
+
+    df_lcps_country = df_lcps.copy()
+    df_lcps_country['Nederland'] = df_lcps['Aantal'] - df_lcps['AantalDuitsland']
+    df_lcps_country['Duitsland'] = df_lcps['AantalDuitsland']
+    df_lcps_country = df_lcps_country[["Nederland", "Duitsland"]].stack(dropna=False)
+    df_lcps_country.index.names = ['Datum', 'Land']
+    df_lcps_country.name = "Aantal"
+    df_lcps_country = df_lcps_country.to_frame()
+
+    df_lcps_country_from_file = pd.read_csv('data/lcps_ic_country.csv', index_col=[0, 1])
+
+    df_lcps_country = df_lcps_country_from_file.combine_first(df_lcps_country)
+    df_lcps_country[~df_lcps_country.index.duplicated()]
+    df_lcps_country['Aantal'] = df_lcps_country['Aantal'].astype(pd.Int64Dtype())
+
+    df_lcps_country[["Aantal"]].to_csv('data/lcps_ic_country.csv')
