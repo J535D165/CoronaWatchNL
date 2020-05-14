34f4e31f496c703341fb90c2150d9e49b826ff3a
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-05-14 14:26:57 +0200

    Add data and plots 2020-05-14

--- data_collection.sh
+++ data_collection.sh
@@ -5,7 +5,7 @@ python workflows/rivm/data_rivm_geo.py
 python workflows/json/json-api.py
 python workflows/rivm/merge_website_charts.py
 python workflows/nice/nice_download_merge.py
-pdftotext reports/COVID-19_epidemiological_report_20200513.pdf
+pdftotext reports/COVID-19_epidemiological_report_20200514.pdf
 python workflows/rivm_pdf/parse_pdf_table.py
 
 
