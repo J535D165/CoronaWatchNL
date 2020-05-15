f1935d571fd4a8d9a86ef589e69c978105216feb
RVoor <63646486+RVoor@users.noreply.github.com>
2020-05-15 11:07:25 +0200

    Add data-ic and corresponding workflow (#112)

---
+++ workflows/nice/data_nice_lcps_ic.py
@@ -0,0 +1,220 @@
+from pathlib import Path
+import itertools
+
+import pandas as pd
+import numpy as np
+
+#from utils import convert_to_int
+import utils
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+
+DATA_FOLDER = Path("data-ic")
+
+def get_timeline():
+
+    df = pd.read_csv(Path("data", "nice_ic_by_day.csv"))
+    dates = sorted(df["Datum"].unique())
+
+    return dates
+
+
+def export_date(df, data_folder, prefix, data_date=None, label=None):
+
+    if data_date:
+        df_date = df.loc[df["Datum"] == data_date, :]
+    else:
+        df_date = df
+
+    # export with data date
+    if label is not None:
+        export_path = Path(DATA_FOLDER, data_folder, f"{prefix}_{label}.csv")
+    else:
+        export_path = Path(DATA_FOLDER, data_folder, f"{prefix}.csv")
+
+    print(f"Export {export_path}")
+    df_date.to_csv(export_path, index=False)
+
+# NICE data
+VARIABLES = [
+    "icCount",
+    "new",
+    "intakeCount",
+    "intakeCumulative",
+    "survivedCumulative",
+    "diedCumulative",
+    "dischargedTotal"
+]
+
+TYPE = [
+    "Totaal ingezette IC's",
+    "Toename opnamen (IC)",
+    "Totaal opnamen (IC)",
+    "Cumulatief opnamen (IC)",
+    "Cumulatief ontslag (ziekenhuis)",
+    "Cumulatief ontslag (overleden)",
+    "Totaal ontslag (IC)",
+    "Toename ontslag (ziekenhuis)",
+    "Toename ontslag (overleden)",
+]
+
+def main_long_nice():
+
+    df_reported = pd.read_csv(Path("data", "nice_ic_by_day.csv"))
+    df_reported['new'] = df_reported['newIntake'] + df_reported['newSuspected']
+    df_reported['Type'] = 'NA'
+    
+    big = pd.DataFrame([])
+    for i in VARIABLES:
+        new = df_reported[['Datum', i, 'Type']]
+        new = new.rename(columns={i: "Aantal"})
+        new['Type'] = TYPE[VARIABLES.index(i)]
+        big = big.append(new, ignore_index = True)
+    
+    new = pd.DataFrame([])
+    new = big.loc[big['Type'].isin(["Cumulatief ontslag (ziekenhuis)", "Cumulatief ontslag (overleden)"])]
+    new["AantalCumulatief"] = new["Aantal"]
+    new.loc[new["Type"] == "Cumulatief ontslag (ziekenhuis)", "Type"] = "Toename ontslag (ziekenhuis)"
+    new.loc[new["Type"] == "Cumulatief ontslag (overleden)", "Type"] = "Toename ontslag (overleden)"
+    new["Aantal"] = new \
+        .groupby('Type', sort=True)['AantalCumulatief'] \
+        .transform(pd.Series.diff)
+    new.loc[new["Datum"] == "2020-02-27", "Aantal"] = \
+        new.loc[new["Datum"] == "2020-02-27", "AantalCumulatief"]
+    
+    big = big.append(new, sort = False)
+    big = big.sort_values('Datum', ascending=True) 
+    big = big.reset_index(drop=True)
+
+    big['Aantal'] = big["Aantal"].astype(pd.Int64Dtype())
+
+    # format the columns
+    big = big[[
+        "Datum",
+        "Type",
+        "Aantal"
+    ]]
+
+    Path(DATA_FOLDER, "data-nice").mkdir(exist_ok=True)
+
+    # dates = sorted(big["Datum"].unique())
+
+    # export by date
+    # for data_date in dates:
+       # export_date(big, "data-nice", "NICE_NL_IC", data_date, str(data_date).replace("-", ""))
+
+    # export latest day
+    # export_date(big, "data-nice", "NICE_NL_IC", data_date=dates[-1], label="latest")
+
+    # export all (latest download)
+    export_date(big, "data-nice", "NICE_IC_long", data_date=None, label="latest")
+
+TYPES = [
+    "IngezetteICs",
+    "ToenameOpnamen",
+    "TotaalOpnamen",
+    "CumulatiefOpnamen",
+    "CumulatiefOntslagZiekenhuis",
+    "CumulatiefOntslagOverleden",
+    "TotaalOntslagIC",
+    "ToenameOntslagZiekenhuis",
+    "ToenameOntslagOverleden",
+]
+
+def main_wide_nice():
+
+    df_reported = pd.read_csv(Path("data", "nice_ic_by_day.csv"))
+    df_reported['new'] = df_reported['newIntake'] + df_reported['newSuspected']
+    df_reported.drop(['newIntake', 'newSuspected'], axis = 1, inplace = True)
+    df_reported.rename(columns={
+                       VARIABLES[0]: TYPES[0], 
+                       VARIABLES[1]: TYPES[1],
+                       VARIABLES[2]: TYPES[2],
+                       VARIABLES[3]: TYPES[3],
+                       VARIABLES[4]: TYPES[4],
+                       VARIABLES[5]: TYPES[5],
+                       VARIABLES[6]: TYPES[6]}, inplace=True)
+    
+    df_reported["ToenameOntslagOverleden"] = df_reported["CumulatiefOntslagOverleden"]
+    df_reported["ToenameOntslagOverleden"] = df_reported \
+        ['CumulatiefOntslagOverleden'] \
+        .transform(pd.Series.diff)
+    df_reported['ToenameOntslagOverleden'] = df_reported["ToenameOntslagOverleden"].astype(pd.Int64Dtype())
+    
+    df_reported["ToenameOntslagZiekenhuis"] = df_reported["CumulatiefOntslagZiekenhuis"]
+    df_reported["ToenameOntslagZiekenhuis"] = df_reported \
+        ['CumulatiefOntslagZiekenhuis'] \
+        .transform(pd.Series.diff)
+    df_reported['ToenameOntslagZiekenhuis'] = df_reported["ToenameOntslagZiekenhuis"].astype(pd.Int64Dtype())
+
+ 
+    # format the columns
+    df_reported = df_reported[[
+        "Datum",
+        "IngezetteICs",
+        "TotaalOpnamen",
+        "ToenameOpnamen",
+        "CumulatiefOpnamen",
+        "ToenameOntslagZiekenhuis",
+        "CumulatiefOntslagZiekenhuis",
+        "ToenameOntslagOverleden",
+        "CumulatiefOntslagOverleden",
+        "TotaalOntslagIC"
+    ]]
+
+    Path(DATA_FOLDER, "data-nice").mkdir(exist_ok=True)
+    
+    # export all (latest download)
+    export_date(df_reported, "data-nice", "NICE_IC_wide", data_date=None, label="latest")
+
+    
+# LCPS data
+def main_lcps():
+
+    df_reported = pd.read_csv(Path("data", "lcps_ic_country.csv"))    
+    df_reported['Aantal'] = df_reported["Aantal"].astype(pd.Int64Dtype())
+
+    dates = sorted(df_reported["Datum"].unique())
+
+    for i in dates:
+        data = {'Datum':  [i],
+        'Land': ['Totaal'], 'Aantal':['NA']}
+        new = pd.DataFrame(data, columns = ['Datum','Land','Aantal'])
+        new['Aantal'] = sum(df_reported.loc[df_reported['Datum'] == i, 'Aantal'])
+        df_reported = df_reported.append(new, ignore_index = True)
+
+    df_reported = df_reported.sort_values('Datum', ascending=True)    
+    df_reported['Aantal'] = df_reported["Aantal"].astype(pd.Int64Dtype())
+
+    # format the columns
+    df_reported = df_reported[[
+        "Datum",
+        "Land",
+        "Aantal"
+    ]]
+
+    Path(DATA_FOLDER, "data-lcps").mkdir(exist_ok=True)
+
+    # export by date
+    # for data_date in dates:
+        # export_date(df_reported, "data-lcps", "LCPS_NL_IC", data_date, str(data_date).replace("-", ""))
+
+    # export latest day
+    # export_date(df_reported, "data-lcps", "LCPS_NL_IC", data_date=dates[-1], label="latest")
+
+    # export all (latest)
+    export_date(df_reported, "data-lcps", "LCPS_IC", data_date=None, label="latest")
+        
+    
+if __name__ == '__main__':
+
+    DATA_FOLDER.mkdir(exist_ok=True)
+
+    main_long_nice()
+    
+    main_wide_nice()
+     
+    main_lcps()
+
+
--- workflows/nice/nice_download_merge.py
+++ workflows/nice/nice_download_merge.py
@@ -61,34 +61,50 @@ if __name__ == '__main__':
         download_api_nice(url=value, name=key)
 
     # get data
+    # icCount: number of ICs with at least 1 COVID-19 case on date of notification
     df_ic_count = pd.read_json(Path(RAW_DATA_FOLDER, f"ic-count-{datetime.date.today()}.json")) \
         .rename({"date": "Datum", "value": "icCount"}, axis=1) \
         .set_index("Datum")
 
+    # intakeCount: number of COVID-19 cases taken into the IC on date of notification
     df_intake_count = pd.read_json(Path(RAW_DATA_FOLDER, f"intake-count-{datetime.date.today()}.json")) \
         .rename({"date": "Datum", "value": "intakeCount"}, axis=1) \
         .set_index("Datum")
 
+    # intakeCumulative: total number of COVID-19 cases that have been taken into the IC up to the date of notification 
     df_intake_cum = pd.read_json(Path(RAW_DATA_FOLDER, f"intake-cumulative-{datetime.date.today()}.json")) \
         .rename({"date": "Datum", "value": "intakeCumulative"}, axis=1) \
         .set_index("Datum")
 
+    # newIntake: number of new COVID-19 cases taken into the IC on the date of notification
     df_new_intake_count, df_new_intake_susp = open_multi_dim_table(
         Path(RAW_DATA_FOLDER, f"new-intake-{datetime.date.today()}.json")
     )
     df_new_intake_count = df_new_intake_count\
         .rename({"date": "Datum", "value": "newIntake"}, axis=1) \
         .set_index("Datum")
-
-    df_died, df2, df_survived = open_multi_dim_table(
+        
+    # newSuspected: number of new suspected COVID-19 cases taken into the IC on the date of notification   
+    df_new_intake_susp = df_new_intake_susp\
+        .rename({"date": "Datum", "value": "newSuspected"}, axis=1) \
+        .set_index("Datum")
+           
+    # diedCumulative: total number of IC COVID-19 cases that died up to the date of notification
+    df_died, df_survived, df_ontslagen = open_multi_dim_table(
         Path(RAW_DATA_FOLDER, f"died-and-survivors-cumulative-{datetime.date.today()}.json")
     )
     df_died = df_died \
         .rename({"date": "Datum", "value": "diedCumulative"}, axis=1) \
         .set_index("Datum")
 
+    # survivedCumulative: total number of IC COVID-19 cases that walked out of hospital alive up to the date of notification
     df_survived = df_survived \
         .rename({"date": "Datum", "value": "survivedCumulative"}, axis=1) \
+        .set_index("Datum")
+    
+    # dischargedCumulative: total number of IC COVID-19 cases that were discharged from the IC up to the date of notification    
+    df_ontslagen = df_ontslagen \
+        .rename({"date": "Datum", "value": "dischargedTotal"}, axis=1) \
         .set_index("Datum")
 
     df_result = pd.concat([
@@ -98,6 +114,8 @@ if __name__ == '__main__':
         df_intake_cum,
         df_survived,
         df_died,
+        df_new_intake_susp,
+        df_ontslagen
     ], axis=1)
 
     print(df_result.head())
