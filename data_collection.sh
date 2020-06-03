dd2a6cdac63dce4b862bdddeeec1a8e163bf4ce2
RVoor <63646486+RVoor@users.noreply.github.com>
2020-06-03 11:51:29 +0200

    Add automatisation data update

--- data_collection.sh
+++ data_collection.sh
@@ -1,3 +1,5 @@
+python workflows/rivm_pdf/parse_pdf_table.py
+python workflows/rivm_pdf/data_rivm_pfd_misc.py
 python workflows/rivm/depr.py
 python workflows/rivm/download_rivm_map_data.py
 python workflows/rivm/merge_data.py
@@ -8,7 +10,4 @@ python workflows/nice/nice_download_merge.py
 python workflows/nice/data-ic_nice.py
 python workflows/lcps/data-ic_lcps.py
 pdftotext reports/COVID-19_epidemiological_report_20200601.pdf
-python workflows/rivm_pdf/parse_pdf_table.py
 python workflows/rivm/data_rivm_desc.py
-
-
--- workflows/rivm_pdf/parse_pdf_table.py
+++ workflows/rivm_pdf/parse_pdf_table.py
@@ -8,34 +8,14 @@ import pandas as pd
 DATE = datetime.date(2020, 6, 2)
 
 AGES = ["0-4", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", "80-84", "85-89", "90-94", "95+", "Niet vermeld"]
-PROVINCIES = ["Drenthe", "Flevoland", "Friesland", "Gelderland", "Groningen", "Limburg", "Noord-Brabant", "Noord-Holland", "Overijssel", "Utrecht", "Zeeland", "Zuid-Holland"]
+PROVINCIES = ["Groningen", "Friesland", "Drenthe", "Overijssel", "Flevoland", "Gelderland", "Utrecht", "Noord-Holland", "Zuid-Holland", "Zeeland", "Noord-Brabant", "Limburg"]
 GENDER = ["Man", "Vrouw", "Niet vermeld"]
 ONDERLIGGEND_A = ["Totaal gemeld","Onderliggende aandoening en/of zwangerschap","Geen onderliggende aandoening","Niet vermeld"]
 ONDERLIGGEND_B = ["Zwangerschap", "Cardio-vasculaire aandoeningen en hypertensie", "Diabetes", "Leveraandoening", "Chronische neurologische of neuromusculaire aandoeningen", "Immuundeficientie", "Nieraandoening", "Chronische longaandoeningen", "Maligniteit", "Overig"]
 
 
 #### Data-misc
-DATA_FOLDER = Path("data-misc/data-underlying")
-
-def export_date(df, data_folder, prefix, data_date=None, label=None):
-
-    if data_date:
-        df_date = df.loc[df["Datum"] == data_date, :]
-    else:
-        df_date = df
-
-    # export with data date
-    if label is not None:
-        export_path = Path(DATA_FOLDER, data_folder, f"{prefix}_{label}.csv")
-    else:
-        export_path = Path(DATA_FOLDER, data_folder, f"{prefix}.csv")
-
-    print(f"Export {export_path}")
-    df_date.to_csv(export_path, index=False)
-    
-
 def parse_onderliggende_a(content):
-    DATA_FOLDER = Path("data-misc/data-underlying")
 
     onderliggend_a = re.findall(
         r"Totaal gemeld\n"
@@ -65,16 +45,15 @@ def parse_onderliggende_a(content):
         df = df.sort_values(by='Datum')   
         df = df.reset_index(drop=True)
         
-        Path(DATA_FOLDER, "data-underlying_statistics").mkdir(exist_ok=True)
-       
-        # export all
-        export_date(df, "data-underlying_statistics", "RIVM_NL_deceased_under70_statistics", data_date=None, label=None)
+        export_path = Path("data-misc/data-underlying", "data-underlying_statistics", "RIVM_NL_deceased_under70_statistics.csv")
+        print(f"Export {export_path}")
+        df.to_csv(export_path, index=False)
+        
     else:
         print("Selected date is already in statistics file")
 
 
 def parse_onderliggende_b(content):
-    DATA_FOLDER = Path("data-misc/data-underlying")
 
     onderliggend_b = re.findall(
         r"Nieraandoening\n"
@@ -109,39 +88,211 @@ def parse_onderliggende_b(content):
         df = df.sort_values(by='Datum')   
         df = df.reset_index(drop=True)
         
-        Path(DATA_FOLDER, "data-underlying_conditions").mkdir(exist_ok=True)
+        export_path = Path("data-misc/data-underlying", "data-underlying_conditions", "RIVM_NL_deceased_under70_conditions.csv")
+        print(f"Export {export_path}")
+        df.to_csv(export_path, index=False)
         
-        # export all
-        export_date(df, "data-underlying_conditions", "RIVM_NL_deceased_under70_conditions", data_date=None, label=None)
-
     else:
         print("Selected date is already in conditions file")
 
 
 ### Data-desc 
-DATA_FOLDER = Path("data")
-
 def parse_age(content):
-    
-    DATA_FOLDER = Path("data")
 
-    age_match = re.findall(r"Totaal gemeld\n0-4\n5-9\n10-14\n15-19\n20-24\n25-29\n30-34\n35-39\n40-44\n45-49\n50-54\n55-59\n60-64\n65-69\n70-74\n75-79\n80-84\n85-89\n90-94\n95\+\nNiet vermeld\n\n\d+\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n.*\nZiekenhuisopname\n\d+\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n.*\nOverleden\n\d+\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)\n(\d+)", content, re.MULTILINE)
+    age_match = re.findall(
+        r"Leeftijdsgroep\n\n"
+    	r"Totaal gemeld\n\n"
+    	r"Totaal gemeld\n"
+    	r"0-4\n"
+    	r"5-9\n"
+    	r"10-14\n"
+    	r"15-19\n"
+    	r"20-24\n"
+    	r"25-29\n"
+    	r"30-34\n"
+    	r"35-39\n"
+    	r"40-44\n"
+    	r"45-49\n"
+    	r"50-54\n"
+    	r"55-59\n"
+    	r"60-64\n"
+    	r"65-69\n"
+    	r"70-74\n"
+    	r"75-79\n"
+    	r"80-84\n"
+    	r"85-89\n"
+    	r"90-94\n"
+    	r"95\+\n"
+    	r"Niet vermeld\n\n"
+    	r"\d+\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n\n"
+    	r"%\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n\n"
+    	r"Ziekenhuisopname\n"
+    	r"\d+\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n\n"
+    	r"%\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n\n"
+    	r"Overleden\n"
+    	r"\d+\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n"
+    	r"(\d+)\n\n"
+    	r"%\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d\n"
+    	r"\d+\.\d",
+        content)
+    
     assert len(age_match[0]) == 63
 
+    new = pd.DataFrame()
     for i, v in enumerate(age_match[0][0:21]):
-        print(f"{DATE},{AGES[i]},Totaal,{v}")
+        data = [(f"{DATE}", f"{AGES[i]}", "Totaal",f"{v}")]
+        new = new.append(data, ignore_index=True)
 
     for i, v in enumerate(age_match[0][21:42]):
-        print(f"{DATE},{AGES[i]},Ziekenhuisopname,{v}")
+        data = [(f"{DATE}", f"{AGES[i]}", "Ziekenhuisopname",f"{v}")]
+        new = new.append(data, ignore_index=True)
 
     for i, v in enumerate(age_match[0][42:63]):
-        print(f"{DATE},{AGES[i]},Overleden,{v}")
+        data = [(f"{DATE}", f"{AGES[i]}", "Overleden",f"{v}")]
+        new = new.append(data, ignore_index=True)
         
+    new = new.rename(columns={0: 'Datum', 1: 'LeeftijdGroep', 2: 'Type', 3: 'Aantal'})
+
+    df = pd.read_csv(Path("data", "rivm_NL_covid19_age.csv"), encoding = "utf8")
+    
+    if f"{DATE}" not in str(df['Datum']):
+        df = df.append(new, ignore_index = True)
+        df = df.reset_index(drop=True)
+        
+        export_path = Path("data", "rivm_NL_covid19_age.csv")
+        print(f"Export {export_path}")
+        df.to_csv(export_path, index=False)
+
+    else:
+        print("Selected date is already in age file")
+   
 
 def parse_gender(content):
     
-    DATA_FOLDER = Path("data")
-
     gender = re.findall(
         r"Geslacht\n"
         r"Totaal gemeld\n"
@@ -188,7 +339,6 @@ def parse_gender(content):
         data = [(f"{DATE}", f"{GENDER[i]}", "Overleden",f"{v}")]
         new = new.append(data, ignore_index=True)
     
-
     new = new.rename(columns={0: 'Datum', 1: 'Geslacht', 2: 'Type', 3: 'Aantal'})
 
     df = pd.read_csv(Path("data", "rivm_NL_covid19_sex.csv"), encoding = "utf8")
@@ -197,10 +347,9 @@ def parse_gender(content):
         df = df.append(new, ignore_index = True)
         df = df.reset_index(drop=True)
         
-        Path(DATA_FOLDER).mkdir(exist_ok=True)
-        
-        # export all
-        export_date(df, "", "rivm_NL_covid19_sex", data_date=None, label=None)
+        export_path = Path("data", "rivm_NL_covid19_sex.csv")
+        print(f"Export {export_path}")
+        df.to_csv(export_path, index=False)
 
     else:
         print("Selected date is already in sex file")
@@ -237,9 +386,27 @@ def parse_provincie(content):
         r"(\d+)", content, re.MULTILINE)
 
     assert len(provincie_match[0]) == 12
-
+        
+    new = pd.DataFrame()
     for i, v in enumerate(provincie_match[0]):
-        print(f"{DATE},{PROVINCIES[i]},{v}")
+        data = [(f"{DATE}",f"{PROVINCIES[i]}",f"{v}")]
+        new = new.append(data, ignore_index=True)
+        
+    new = new.rename(columns={0: 'Datum', 1: 'Provincienaam', 2: 'Aantal'})
+        
+    df = pd.read_csv(Path("data", "rivm_NL_covid19_province.csv"), encoding = "utf8")
+    
+    if f"{DATE}" not in str(df['Datum']):
+        df = df.append(new, ignore_index = True)
+        df = df.sort_values(by='Datum')   
+        df = df.reset_index(drop=True)
+        
+        export_path = Path("data", "rivm_NL_covid19_province.csv")
+        print(f"Export {export_path}")
+        df.to_csv(export_path, index=False)
+        
+    else:
+        print("Selected date is already in conditions file")
 
 
 def parse_test_report(content):
@@ -307,24 +474,6 @@ if __name__ == '__main__':
     
     parse_gender(content)
 
-    # parse_age(content)
-
-    # print("\n\n\n\n")
-    
-    parse_gender(content)
+    parse_age(content)
 
     parse_provincie(content)
-
-    print("\n\n\n\n")
-
-    
-
-    print("\n\n\n\n")
-
-    df = pd.DataFrame(parse_test_report(content))
-    df["Totaal"] = df["Totaal"].cumsum()
-    df["Positief"] = df["Positief"].cumsum()
-    df = df.set_index(["Datum", "Labs"]).stack().reset_index()
-    df.columns = ['Datum', 'Labs', 'Type', "Aantal"]
-    df["PublicatieDatum"] = DATE
-    print(df[['PublicatieDatum', 'Datum', 'Labs', 'Type', "Aantal"]].to_csv(index=False))
