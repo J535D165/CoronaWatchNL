49356b7c0a50a5107e1b1722981ae52469878e80
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-06-06 14:13:03 +0200

    Add data and plots 2020-06-06

--- data_collection.sh
+++ data_collection.sh
@@ -1,3 +1,5 @@
+d=`date +%Y%m%d`
+
 python workflows/rivm/depr.py
 python workflows/rivm/download_rivm_map_data.py
 python workflows/rivm/merge_data.py
@@ -7,7 +9,7 @@ python workflows/rivm/merge_website_charts.py
 python workflows/nice/nice_download_merge.py
 python workflows/nice/data-ic_nice.py
 python workflows/lcps/data-ic_lcps.py
-pdftotext reports/COVID-19_epidemiological_report_20200605.pdf
+pdftotext reports/COVID-19_epidemiological_report_${d}.pdf
 python workflows/rivm_pdf/parse_pdf_table.py
 python workflows/rivm/data_rivm_desc.py
 python workflows/rivm_pdf/data_rivm_pdf_misc.py
