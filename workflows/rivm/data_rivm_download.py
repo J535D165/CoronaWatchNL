5eec6aa82bcace3566c80abef5155be8a32949e7
RVoor <63646486+RVoor@users.noreply.github.com>
2020-06-30 15:32:28 +0200

    Change to extract from gemeente_cumulatief.json

--- workflows/rivm/data_rivm_download.py
+++ workflows/rivm/data_rivm_download.py
@@ -1,18 +1,17 @@
 
 """Download RIVM daily stats"""
 
-from pathlib import Path
-import re
-
-import requests
-from lxml import html
-import lxml.html
 import pandas as pd
-from datetime import datetime
+import urllib.request, json 
+from datetime import date
+from pathlib import Path
+import itertools
+import datetime
+import numpy as np
 
 # global variables
-RIVM_DATA_BASE_CORONA_URL = "https://www.rivm.nl/coronavirus-covid-19/actueel"
-
+RIVM_DATA_BASE_CORONA_URL = "https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.json"
+DATE = datetime.date.today()
 
 def verify_dataset():
     pass
@@ -22,49 +21,34 @@ def download_rivm_data():
 
     verify_dataset()
 
-    # download data
-    doc = requests.get(RIVM_DATA_BASE_CORONA_URL)
-    root = lxml.html.fromstring(doc.content)
-    
-    # obtain date
-    DATE = str(re.search(r'Actuele cijfers COVID-19: (\d+) [a-zA-Z]+ (\d+)', doc.content.decode()))
-    DATE = DATE.split(": ")[1]
-    DATE = DATE.split("'>")[0]
-    
-    Maand = {'januari':1, 'februari':2, 'maart':3, 'april':4, 'mei':5, 'juni':6, 'juli':7, 'augustus':8, 'september':9, 'oktober':10, 'november':11, 'december':12} 
-    maand = ''.join(re.findall('[a-zA-Z]+', DATE))
+    with urllib.request.urlopen(RIVM_DATA_BASE_CORONA_URL) as url:
+        data = json.loads(url.read().decode())
+        
+    # Sum all municipalities to get total sum
+    total = 0
+    hospital = 0
+    over = 0
+    print("Loading data...")
+    for i in range(len(data)):
+        if f'{DATE}' in data[i]['Date_of_report']:
+            total = total + data[i]['Total_reported']
+            hospital = hospital + data[i]['Hospital_admission']
+            over = over + data[i]['Deceased']
 
-    DATE = DATE.replace(maand, str(Maand[maand]))
-    DATE = datetime.strptime(DATE, '%d %m %Y')
-    DATE = DATE.strftime("%Y-%m-%d")
     
-    # obtain table data
-    root.xpath('//tr/td//text()')
-    for tbl in root.xpath('//table'):
-        elements = tbl.xpath('.//tr/td//text()')
-    data = str(elements)
-    data = re.findall('(\d+\.\d{1,3})', data)
-    
-    # create new dataframe
-    types = ['Totaal', 'Ziekenhuisopname', 'Overleden']
-    df = pd.DataFrame()
-    for i in range(3):
-        aantal = data[i].replace(".", "")
-        new = {'Datum': DATE, 'Type': types[i], 'Aantal': aantal}
-        df = df.append(new, ignore_index = True)
-
-    df = df[[
-        "Datum",
-        "Type",
-        "Aantal"
-    ]]
+    # Create new dataframe
+    mydata = {'Datum':  [f'{DATE}', f'{DATE}', f'{DATE}'],
+        'Type': ['Totaal', 'Ziekenhuisopname', 'Overleden'],
+        'Aantal': [total, hospital, over],
+        }
+    df = pd.DataFrame(mydata)
     
     # Add new data to datafile data-folder
     df_reported = pd.read_csv(Path("data", "rivm_NL_covid19_national.csv"))
     df_reported['Aantal'] = df_reported["Aantal"].astype(pd.Int64Dtype())
 
     
-    if DATE not in str(df_reported['Datum']):
+    if f'{DATE}' not in str(df_reported['Datum']):
         df_reported = df_reported.append(df, ignore_index = True)
         df_reported = df_reported.reset_index(drop=True)
 
