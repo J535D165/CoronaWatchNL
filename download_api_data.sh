b68078ee7dcacfcc48e000bf22060f7a93a22dd7
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-07-04 14:26:36 +0200

    Add NICE data

--- download_api_data.sh
+++ download_api_data.sh
@@ -1,5 +1,5 @@
 d=`date +%Y-%m-%d`
-d="2020-07-02"
+d="2020-07-03"
 
 curl https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.json > raw_data/api_data/COVID-19_aantallen_gemeente_cumulatief-${d}.json
 curl https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.csv > raw_data/api_data/COVID-19_aantallen_gemeente_cumulatief-${d}.csv
