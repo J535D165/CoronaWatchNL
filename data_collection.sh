0ab55f7b51b844f352aa699650c14611b96da93b
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-07-28 19:02:10 +0200

    Add data and plots 2020-07-28

--- data_collection.sh
+++ data_collection.sh
@@ -11,9 +11,9 @@
 # python workflows/nice/nice_download_merge.py
 # python workflows/nice/data-ic_nice.py
 
-# d=`date +%Y%m%d`
-# pdftotext reports/COVID-19_epidemiological_report_${d}.pdf
-# python workflows/rivm_pdf/parse_pdf_table.py
+d=`date +%Y%m%d`
+pdftotext reports/COVID-19_epidemiological_report_${d}.pdf
+python workflows/rivm_pdf/parse_pdf_table.py
 # python workflows/rivm_pdf/data_rivm_pdf_misc.py
 # python workflows/rivm/data_rivm_desc.py
 # python tests.py
