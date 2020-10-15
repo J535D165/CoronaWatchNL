47ded099883f86c255c87899045294f8242fcd34
Sem Voigtländer <43578852+userlandkernel@users.noreply.github.com>
2020-10-15 22:51:31 +0200

    Update download_api_data.sh

--- download_api_data.sh
+++ download_api_data.sh
@@ -1,7 +1,12 @@
+#!/usr/bin/env bash
+
 d=`date +%Y-%m-%d`
 d="2020-07-03"
 
-curl https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.json > raw_data/api_data/COVID-19_aantallen_gemeente_cumulatief-${d}.json
-curl https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.csv > raw_data/api_data/COVID-19_aantallen_gemeente_cumulatief-${d}.csv
-curl https://data.rivm.nl/covid-19/COVID-19_casus_landelijk.json > raw_data/api_data/COVID-19_casus_landelijk-${d}.json
-curl https://data.rivm.nl/covid-19/COVID-19_casus_landelijk.csv > raw_data/api_data/COVID-19_casus_landelijk-${d}.csv
+RIVM_CDN="https://data.rivm.nl/"
+COVID_CDN="$RIVM_CDN/covid-19"
+
+curl "$COVID_CDN/COVID-19_aantallen_gemeente_cumulatief.json" -o "raw_data/api_data/COVID-19_aantallen_gemeente_cumulatief-${d}.json"
+curl "$COVID_CDN/COVID-19_aantallen_gemeente_cumulatief.csv" -o "raw_data/api_data/COVID-19_aantallen_gemeente_cumulatief-${d}.csv"
+curl "$COVID_CDN/COVID-19_casus_landelijk.json" -o "raw_data/api_data/COVID-19_casus_landelijk-${d}.json"
+curl "$COVID_CDN/COVID-19_casus_landelijk.csv" -o "raw_data/api_data/COVID-19_casus_landelijk-${d}.csv"
