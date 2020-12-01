026c4ba60cd67a50ba3e2aec7b73d897f2ca45e6
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-12-01 16:26:05 +0100

    Add pdf data and plots 2020-12-01

--- data_collection.sh
+++ data_collection.sh
@@ -16,7 +16,7 @@ python workflows/rivm/merge_website_charts.py
 # Parse reports
 d=`date +%Y%m%d`
 pdftotext reports/COVID-19_epidemiological_report_${d}.pdf
-python workflows/rivm_pdf/parse_pdf_table.py
+# python workflows/rivm_pdf/parse_pdf_table.py
 # python workflows/rivm_pdf/data_rivm_pdf_misc.py
 # python workflows/rivm/data_rivm_desc.py
 # python tests.py
